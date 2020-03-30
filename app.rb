b6b808a4c78cadb1fccb1d535d34cffc3913a3cf
tomoaki-kimura <tkim19750404@gmail.com>
2020-03-30 10:55:58 +0900

    add_3rd_stage

--- app.rb
+++ app.rb
@@ -1,34 +1,39 @@
 require 'sinatra'
+enable :sessions
 
 get '/' do
   @places = place_data
-  data = @places.first
-  @title = data[:place]
-  @latitude = data[:latitude]
-  @longitude = data[:longitude]
-  @memo = data[:memo]
+  index = params[:index] ? params[:index].to_i : 0
+  data = @places[index]
+  p @title = data ? data[:title] : ''
+  p @latitude = data ? data[:latitude].to_f : '0.0'
+  p @longitude = data ? data[:longitude].to_f : '0.0'
+  p @memo = data ? data[:memo] : ''
   erb :index
 end
 
 post '/' do
-  @places = place_data
-  location = params[:location].split(',')
   @title = params[:title]
-  @latitude = location[0]
-  @longitude = location[1]
-  @memo = location[2]
+  @latitude = params[:latitude]
+  @longitude = params[:longitude]
+  @memo = params[:memo]
+  @places = place_data << { title: @title, latitude: @latitude , longitude: @longitude, memo: @memo }
   erb :index
+end
+
+post '/delete' do
+  clear_place_data
+  redirect '/'
 end
 
 private
 
 def place_data
-  [
-    { title: "新宿区", latitude: "35.68944", longitude: "139.69167", memo: "東京の県庁所在地です" },
-    { title: "札幌市", latitude: "40.82444", longitude: "140.74", memo: "雪まつりの会場です" },
-    { title: "大阪市", latitude: "34.68639", longitude: "135.52", memo: "たこやき食べたい" },
-    { title: "福岡市", latitude: "33.60639", longitude: "130.41806", memo: "博多どんたくがあります" }
-  ]
+  session[:place_data] || session[:place_data] = []
+end
+
+def clear_place_data
+  session[:place_data] = []
 end
 
 
