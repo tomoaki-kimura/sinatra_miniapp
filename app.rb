f537da4f6303abfc8466a486edab0f0483088bec
tomoaki-kimura <tkim19750404@gmail.com>
2020-03-30 10:55:58 +0900

    for text 3rd_stage

--- app.rb
+++ app.rb
@@ -1,34 +1,33 @@
 require 'sinatra'
+enable :sessions
 
 get '/' do
   @places = place_data
-  data = @places.first
-  @title = data[:place]
-  @latitude = data[:latitude]
-  @longitude = data[:longitude]
-  @memo = data[:memo]
+  index = params[:index] ? params[:index].to_i : 0
+  data = @places[index]
+  @title = data ? data[:title] : ''
+  @latitude = data ? data[:latitude].to_f : '0.0'
+  @longitude = data ? data[:longitude].to_f : '0.0'
+  @memo = data ? data[:memo] : ''
   erb :index
 end
 
 post '/' do
-  @places = place_data
-  location = params[:location].split(',')
-  @title = params[:title]
-  @latitude = location[0]
-  @longitude = location[1]
-  @memo = location[2]
-  erb :index
+  #ここに投稿するコードを書きます
+end
+
+post '/delete' do
+  #ここに削するコードを書きます
 end
 
 private
 
 def place_data
-  [
-    { title: "新宿区", latitude: "35.68944", longitude: "139.69167", memo: "東京の県庁所在地です" },
-    { title: "札幌市", latitude: "40.82444", longitude: "140.74", memo: "雪まつりの会場です" },
-    { title: "大阪市", latitude: "34.68639", longitude: "135.52", memo: "たこやき食べたい" },
-    { title: "福岡市", latitude: "33.60639", longitude: "130.41806", memo: "博多どんたくがあります" }
-  ]
+  session[:place_data] || session[:place_data] = []
+end
+
+def clear_place_data
+  session[:place_data] = []
 end
 
 
