b8df0e6ef9254a287163905c03ddc2994b88a681
tomoaki-kimura <tkim19750404@gmail.com>
2020-04-03 18:20:29 +0900

    for text 2nd_stage

--- app.rb
+++ app.rb
@@ -24,10 +24,7 @@ private
 
 def place_data
   [
-    { title: "新宿区", latitude: "35.68944", longitude: "139.69167", memo: "東京の県庁所在地です" },
-    { title: "札幌市", latitude: "40.82444", longitude: "140.74", memo: "雪まつりの会場です" },
-    { title: "大阪市", latitude: "34.68639", longitude: "135.52", memo: "たこやき食べたい" },
-    { title: "福岡市", latitude: "33.60639", longitude: "130.41806", memo: "博多どんたくがあります" }
+    #ここにデーターが入る
   ]
 end
 
